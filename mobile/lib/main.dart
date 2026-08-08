e946832c7d958dc54e8ba7d03b795e3bf2d97260
Burakhan <burakhangultoplar@gmail.com>
2026-08-08 11:05:40 +0300

    Flutter: Firebase entegrasyonu (firebase_core baglandi)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,9 +1,15 @@
 // lib/main.dart
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/firebase_options.dart';
 import 'models/wardrobe_item.dart';
 import 'services/api_service.dart';
 
-void main() => runApp(const LuviaApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const LuviaApp());
+}
 
 class LuviaApp extends StatelessWidget {
   const LuviaApp({super.key});
